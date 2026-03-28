d8b4918bc2c2d885a27aebd825e3c02e56ea2012
unknown <luisfelipe.fsk@gmail.com>
2026-03-28 16:09:27 -0300

    feat(): control unit

---
+++ control_unit.v
@@ -0,0 +1,34 @@
+module control_unit (
+    input [5:0] opcode,
+    output reg_dst,
+    output branch,
+    output mem_read,
+    output mem_write,
+    output load_mem,
+    output alu_src,
+    output reg_write,
+    output [1:0] alu_op
+);
+
+reg [8:0] control_bus;
+assign reg_dst = control_bus[8];
+assign branch = control_bus[7];
+assign mem_read = control_bus[6];
+assign mem_write = control_bus[5];
+assign load_mem = control_bus[4];
+assign alu_src = control_bus[3];
+assign reg_write = control_bus[2];
+assign alu_op = control_bus[1:0];
+
+always @* begin
+    case (opcode)
+        6'b000000: control_bus = 9'b100000110;
+        6'b001000: control_bus = 9'b000001100;
+        6'b100011: control_bus = 9'b001011100;
+        6'b101011: control_bus = 9'b000101000;
+        6'b000100: control_bus = 9'b010000001;
+        default: control_bus = 9'bx;
+    endcase
+end
+
+endmodule
--- imm16_extend.v
+++ imm16_extend.v
@@ -1,16 +1,8 @@
 module imm16_extend (
     input [15:0] imm16,
-    input zero_extend,
     output [31:0] imm32
 );
 
-reg [31:0] ext;
-assign imm32 = ext;
-always @* begin
-    if (zero_extend)
-        ext = {16'b0, imm16};
-    else
-        ext = {{16{imm16[15]}}, imm16};
-end
+assign imm32 = {{16{imm16[15]}}, imm16};
 
 endmodule
